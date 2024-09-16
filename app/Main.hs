d01b56fbc798cd44e119108ab11b71f882c816ce
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2024-09-16 16:06:19 -0700

    CLI for binary tree traversal
    correct amount of commits but I dont it get the init commit properly

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,42 @@
 module Main (main) where
 
-import Lib
+import System.Process (readCreateProcess, shell)
+import System.IO.Temp (withSystemTempDirectory)
+import Control.Monad (forM_)
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Enter the GitHub repository URL:"
+    repoUrl <- getLine
+    putStrLn "Enter the number of commits to process:"
+    n <- readLn :: IO Int
+    
+    withSystemTempDirectory "timelapse" $ \tmpDir -> do
+        putStrLn $ "Cloning repository to " ++ tmpDir
+        cloneRepo tmpDir repoUrl
+        commits <- getCommits tmpDir
+        let selectedCommits = selectCommits n commits
+        putStrLn "Selected commits:"
+        forM_ selectedCommits putStrLn
+
+cloneRepo :: FilePath -> String -> IO ()
+cloneRepo dir url = do
+    putStrLn $ "Cloning " ++ url ++ " into " ++ dir
+    _ <- readCreateProcess (shell $ "git clone " ++ url ++ " " ++ dir) ""
+    return ()
+
+getCommits :: FilePath -> IO [String]
+getCommits dir = do
+    output <- readCreateProcess (shell $ "cd " ++ dir ++ " && git log --format=%H") ""
+    return $ lines output
+
+selectCommits :: Int -> [String] -> [String]
+selectCommits n commits = take n $ binaryTreeTraversal commits
+
+binaryTreeTraversal :: [a] -> [a]
+binaryTreeTraversal [] = []
+binaryTreeTraversal [x] = [x]
+binaryTreeTraversal xs = 
+    let mid = length xs `div` 2
+        (left, m:right) = splitAt mid xs
+    in m : binaryTreeTraversal left ++ binaryTreeTraversal right
