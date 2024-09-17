3c5d4af30554ad04873f241e20d5493980eed031
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2024-09-16 17:22:54 -0700

    runs npm dev on all commits
    fails because forgot to add npm install

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,21 @@
 
 module Main (main) where
 
-import System.Process (readCreateProcess, shell)
+import System.Process
 import System.Directory (createDirectoryIfMissing, removeDirectoryRecursive, doesFileExist)
 import System.FilePath ((</>))
 import System.IO.Temp (withSystemTempDirectory)
-import Control.Monad (forM_)
+import Control.Monad (forM_, unless)
+import Control.Exception (bracket)
+import Control.Concurrent (threadDelay)
 import Data.List (sort)
 import qualified Data.ByteString.Lazy as B
 import Data.Aeson ((.:), decode, Object)
 import Data.Aeson.Types (Parser, parseMaybe)
 import qualified Data.Aeson.KeyMap as KM
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
 main :: IO ()
 main = do
@@ -25,14 +30,16 @@ main = do
         cloneRepo tmpDir repoUrl
         commits <- getCommits tmpDir
         let selectedCommits = selectCommits n commits
-        putStrLn "Selected commits and their package.json scripts:"
+        putStrLn "Selected commits, their package.json scripts, and dev script output:"
         forM_ selectedCommits $ \commit -> do
             putStrLn $ "Commit: " ++ commit
             scripts <- getPackageJsonScripts tmpDir commit
             case scripts of
-                Just s  -> putStrLn $ "Scripts:\n" ++ s
+                Just s  -> do
+                    putStrLn $ "Scripts:\n" ++ s
+                    runDevScript tmpDir
                 Nothing -> putStrLn "No package.json or no scripts found for this commit."
-            putStrLn ""  -- Add a blank line for readability
+            putStrLn ""  
 
 cloneRepo :: FilePath -> String -> IO ()
 cloneRepo dir url = do
@@ -74,7 +81,7 @@ getPackageJsonScripts dir commit = do
             content <- B.readFile packageJsonPath
             case decode content of
                 Just obj -> return $ formatScripts $ parseMaybe (.: "scripts") obj
-                Nothing  -> return $ Just "Error: Could not parse/find package.json"
+                Nothing  -> return $ Just "Error: Could not parse package.json"
         else return Nothing
 
 formatScripts :: Maybe Object -> Maybe String
@@ -82,3 +89,27 @@ formatScripts Nothing = Nothing
 formatScripts (Just scripts) = Just $ unlines $ map formatScript $ KM.toList scripts
   where
     formatScript (name, value) = "  " ++ show name ++ ": " ++ show value
+
+runDevScript :: FilePath -> IO ()
+runDevScript dir = do
+    putStrLn "Running 'npm run dev'..."
+    (_, Just hout, Just herr, ph) <-
+        createProcess (shell "npm run dev") { cwd = Just dir
+                                            , std_out = CreatePipe
+                                            , std_err = CreatePipe
+                                            }
+    
+    bracket (return ph) terminateProcess $ \_ -> do
+        threadDelay 5000000  -- 5 seconds
+        
+        output <- TIO.hGetContents hout
+        error <- TIO.hGetContents herr
+        
+        putStrLn "Initial server output:"
+        TIO.putStrLn $ T.unlines $ take 10 $ T.lines output
+        
+        unless (T.null error) $ do
+            putStrLn "Error output:"
+            TIO.putStrLn error
+        
+    putStrLn "Dev script execution completed."
